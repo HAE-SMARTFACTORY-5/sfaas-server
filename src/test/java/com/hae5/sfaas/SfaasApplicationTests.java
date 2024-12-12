82e5064568c42c072ad2b78076246b6a5bb6cba5
KJBig <kjgi73k@gmail.com>
2024-12-12 12:31:56 +0900

    [HS-9] feat : 프로젝트 기본 설정

---
+++ src/test/java/com/hae5/sfaas/SfaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hae5.sfaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
